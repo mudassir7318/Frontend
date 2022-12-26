b75fbe67ee268d8dd0d328eeb28d09fbeb6b9142
mudassir7318 <mudassirahemed7318@gmail.com>
2022-12-26 06:43:52 +0530

    inital commit

---
+++ javascript/DOM/Simpleform.js
@@ -0,0 +1,14 @@
+function readingParagraph(){
+document.getElementById("demo").innerHTML = "lorem";
+}
+
+function DoubleClick(){
+const collection = document.getElementsByClassName("click");
+collection[0].style.backgroundColor = "red";
+}
+
+function onclick(){
+document.getElementById("btn").setAttribute("class","clickButtonByIdjs");
+
+}
+
---
+++ javascript/click.js
@@ -0,0 +1,9 @@
+function btn(){
+    document.getElementById("btn").style.backgroundColor='aqua'
+}
+function bttn(){
+    document.getElementById("btn1").style.backgroundColor='yellow'
+}
+function button(){
+    document.getElementById("btn2").style.backgroundColor='blue'
+}
